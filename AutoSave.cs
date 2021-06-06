511388466473559986ea5553f303395b2feca3bb
Raphael Frei <raphaelrodriguesfrei@gmail.com>
2021-06-06 19:24:50 -0300

    Added the script via upload
    AutoSave.cs added to the project.

---
+++ AutoSave.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+#endif
+public class AutoSave: MonoBehaviour {
+#if UNITY_EDITOR
+    static AutoSave() {
+        EditorApplication.playmodeStateChanged = () => {
+            if (EditorApplication.isPlayingOrWillChangePlaymode && !EditorApplication.isPlaying) {
+                Debug.Log("Saving before entering Play Mode: " + EditorApplication.currentScene);
+
+                EditorApplication.SaveScene();
+                AssetDatabase.SaveAssets();
+            }
+        };
+    }
+#endif
+}
